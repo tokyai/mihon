36d50e40a3d7791c79e7a4bf068e83a3ec5256d5
tokyai <121705033@qq.com>
2026-09-21 16:26:16 +0800

    Fix missing kotlinx.coroutines.flow.first import in ReaderViewModel

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
@@ -63,6 +63,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
